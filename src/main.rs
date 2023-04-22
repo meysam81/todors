afd3e280ea8ba4a29b53bcee6eaf655133f37d44
Meysam <MeysamAzad81@gmail.com>
2023-04-23 00:34:28 +0700

    refactor CLI interface and back to stable again (#10)
    * refactor CLI interface and back to stable again
    
    * ci: add automated tests
    
    * update cargo
    
    * fix(ci): install mold and show summary

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use cli::{Cli, Commands};
+use cli::{handle_local, Cli, Commands};
 use errors::TodoErrors;
 use logging::{debug, error, info, trace, warn};
 use models::TodoController;
@@ -26,58 +26,16 @@ async fn main() -> Result<(), TodoErrors> {
     trace!(logger, "{:?}", conn);
 
     let todo_controller = TodoController::new(conn);
-    let web_state = http::AppState::new(todo_controller, logger.clone());
+    let web_state = http::AppState::new(todo_controller.clone(), logger.clone());
 
     let cli = Cli::parse();
     debug!(logger, "{:?}", cli);
 
     match cli.command {
-        // Commands::List => {
-        //     let todos = Todo::list(&conn).await?;
-        //     let todos = to_json(&todos)?;
-        //     println!("{}", todos);
-        // }
-        // Commands::Create(cli::Create { title }) => {
-        //     let mut todo = Todo::new(title);
-        //     match todo.save(&conn).await {
-        //         Ok(_) => {
-        //             let todo = to_json(&todo)?;
-        //             println!("{}", todo)
-        //         }
-        //         Err(err) => error!(logger, "Failed to save: {:?}", err),
-        //     };
-        // }
-        // Commands::Delete(cli::Delete { id }) => {
-        //     match Todo::delete(id, &conn).await {
-        //         Ok(_) => info!(logger, "Deleted successfully!"),
-        //         Err(err) => error!(logger, "Failed to delete: {:?}", err),
-        //     };
-        // }
-        // Commands::Update(cli::Update {
-        //     id,
-        //     title,
-        //     done,
-        //     undone,
-        // }) => {
-        //     let done = if let Some(undone) = undone {
-        //         Some(!undone)
-        //     } else {
-        //         done
-        //     };
-        //     match Todo::update(id, title, done, &conn).await {
-        //         Ok(_) => info!(logger, "Updated successfully!"),
-        //         Err(err) => error!(logger, "Failed to update: {:?}", err),
-        //     };
-        // }
-        // Commands::Get(cli::Get { id }) => {
-        //     match Todo::get(id, &conn).await {
-        //         Ok(todo) => {
-        //             let todo = to_json(&todo)?;
-        //             println!("{}", todo)
-        //         }
-        //         Err(err) => error!(logger, "Failed to get: {:?}", err),
-        //     };
-        // }
+        Commands::Local(local) => {
+            let cli_state = cli::CliState::new(todo_controller, logger);
+            handle_local(local, cli_state).await;
+        }
         Commands::Completion(cli::Completion { shell }) => {
             cli::print_completions(shell);
         }
