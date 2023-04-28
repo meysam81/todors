7acaa1e73e302a10740380fc3c595dd450f82996
Meysam Azad <MeysamAzad81@gmail.com>
2023-04-28 10:11:19 +0700

    feat: add batch TODO creation

--- src/traits.rs
+++ src/traits.rs
@@ -27,6 +27,7 @@ pub trait Controller {
     type Output: Serialize;
 
     async fn create(&self, todo: &Self::Input) -> Result<Self::Output, TodoErrors>;
+    async fn create_batch(&self, todos: &[Self::Input]) -> Result<Vec<Self::Id>, TodoErrors>;
     async fn delete(&self, id: Self::Id) -> Result<(), TodoErrors>;
     async fn get(&self, id: Self::Id) -> Result<Self::Output, TodoErrors>;
     async fn list(&self, req: ListRequest) -> Result<ListResponse<Self::Output>, TodoErrors>;
