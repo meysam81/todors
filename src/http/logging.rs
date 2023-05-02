b4eab9071179cf2a6819b5d34a1c74c07b14b6c1
Meysam Azad <MeysamAzad81@gmail.com>
2023-05-02 08:22:27 +0700

    complete the http logging logic by printing machine readable json

--- src/http/logging.rs
+++ src/http/logging.rs
@@ -1,5 +1,6 @@
 use std::future::{ready, Ready};
 
+use crate::serializers::{to_json, Serialize};
 use actix_web::{
     dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform},
     Error as ActixError,
@@ -8,8 +9,28 @@ use futures_util::future::LocalBoxFuture;
 
 #[derive(Default)]
 pub struct LogMiddleware;
+
 pub struct LogMiddlewareService<S> {
     service: S,
+}
+
+#[derive(Serialize, Default)]
+struct Log {
+    timestamp: String,
+    method: String,
+    path: String,
+    http_version: String,
+    client_ip: String,
+    client_real_ip: String,
+    request_headers: String,
+    status_code: String,
+    latency: String,
+}
+
+impl std::fmt::Display for Log {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        to_json(self).unwrap().fmt(f)
+    }
 }
 
 impl<S, B> Transform<S, ServiceRequest> for LogMiddleware
@@ -42,14 +63,43 @@ where
     forward_ready!(service);
 
     fn call(&self, req: ServiceRequest) -> Self::Future {
-        println!("Hi from start. You requested: {}", req.path());
+        let mut log = Log {
+            timestamp: chrono::Utc::now().to_rfc3339_opts(chrono::SecondsFormat::Secs, true),
+            method: req.method().to_string(),
+            path: req.path().to_string(),
+            http_version: format!("{:?}", req.version()),
+            client_ip: req
+                .connection_info()
+                .peer_addr()
+                .unwrap_or_default()
+                .to_string(),
+            client_real_ip: req
+                .connection_info()
+                .realip_remote_addr()
+                .unwrap_or_default()
+                .to_string(),
+            ..Default::default()
+        };
 
         let fut = self.service.call(req);
 
         Box::pin(async move {
+            let start = std::time::Instant::now();
             let res = fut.await?;
+            let elapsed = start.elapsed();
 
-            println!("Hi from response");
+            log.request_headers = res
+                .request()
+                .headers()
+                .iter()
+                .fold(String::new(), |acc, (key, value)| {
+                    format!("{}{}: {}\n", acc, key, value.to_str().unwrap())
+                });
+            log.status_code = res.status().to_string();
+            log.latency = format!("{:?}", elapsed);
+
+            println!("{}", log);
+
             Ok(res)
         })
     }
