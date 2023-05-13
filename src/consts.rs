4c09713c72ea86505f2c1191f04ee0bb38634074
Meysam Azad <MeysamAzad81@gmail.com>
2023-05-13 09:04:30 +0700

    use labels to differentiate the metrics namespace

--- src/consts.rs
+++ src/consts.rs
@@ -3,4 +3,4 @@ pub const DEFAULT_PAGE_LIMIT: u32 = 10;
 pub const DEFAULT_PAGE_HARD_LIMIT: u32 = 1000;
 pub const DEFAULT_CREATE_BATCH_HARD_LIMIT: u32 = 1000;
 pub const DEFAULT_METRICS_URI: &str = "/metrics";
-pub const DEFAULT_METRICS_NAMESPACE: &str = "todors";
+pub const DEFAULT_METRICS_NAMESPACE: &str = "api";
--- src/http/metrics.rs
+++ src/http/metrics.rs
@@ -1,8 +1,13 @@
+use std::collections::HashMap;
+
 use crate::consts;
 
 use actix_web_prometheus::{PrometheusMetrics, PrometheusMetricsBuilder};
 
 pub fn build_metrics() -> PrometheusMetrics {
+    let mut labels = HashMap::new();
+    labels.insert("app".to_string(), "todors".to_string());
+
     PrometheusMetricsBuilder::new(consts::DEFAULT_METRICS_NAMESPACE)
         .endpoint(consts::DEFAULT_METRICS_URI)
         .build()
